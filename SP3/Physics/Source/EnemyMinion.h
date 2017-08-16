d633a08b24a190cc4ec2c47f5a49d722e5df731f
CyndXIII <drakon.lykos@gmail.com>
2017-08-16 09:53:03 +0800

    enemy stuff

--- SP3/Physics/Source/EnemyMinion.h
+++ SP3/Physics/Source/EnemyMinion.h
@@ -12,7 +12,18 @@ public:
 	EnemyMinion() { }
 	~EnemyMinion() { }
 
+	EnemyMinion(int MaxHP, int Atk, float MoveSpd)
+	{
+		this->MaxHP = MaxHP;
+		this->CurrHP = MaxHP;
+		this->Atk = Atk;
+		this->MoveSpd = MoveSpd;
+	}
+
 	//Getters and Setters
+	int getMaxHP() { return MaxHP; }
+	void setMaxHP(int MaxHP) { this->MaxHP = MaxHP; }
+
 	int getCurrHP() { return CurrHP; }
 	void setCurrHP(int HP) { this->CurrHP = HP; }
 
--- SP3/Physics/Source/EnemyTower.h
+++ SP3/Physics/Source/EnemyTower.h
@@ -11,7 +11,17 @@ public:
 	EnemyTower() { }
 	~EnemyTower() { }
 
+	EnemyTower(int MaxHP, float SpawnRate)
+	{
+		this->MaxHP = MaxHP;
+		this->CurrHP = MaxHP;
+		this->SpawnRate = SpawnRate;
+	}
+
 	//Getters and Setters
+	int getMaxHP() { return MaxHP; }
+	void setMaxHP(int MaxHP) { this->MaxHP = MaxHP; }
+
 	int getCurrHP() { return CurrHP; }
 	void setCurrHP(int HP) { this->CurrHP = HP; }
 
