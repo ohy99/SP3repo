f04c5e4b548f8dc3bd30295fa58ebf2a7b4d0c29
ohy <ohy99@hotmail.com>
2017-08-16 15:47:01 +0800

    projectiles and objectpool

--- SP3/Physics/Source/Collidable.h
+++ SP3/Physics/Source/Collidable.h
@@ -1,1 +1,19 @@
-#pragma once
+#ifndef COLLIDABLE_H
+#define COLLIDABLE_H
+
+#include "GameObject.h"
+#include "Faction.h"
+
+class Collidable : public GameObject
+{
+	Faction faction;
+public:
+	Faction::FACTION_SIDE get_faction_side();
+	void set_faction_side(Faction::FACTION_SIDE side);
+	virtual void collision_response(Collidable* obj);
+protected:
+	Collidable();
+	virtual ~Collidable();
+};
+
+#endif // !COLLIDABLE_H
